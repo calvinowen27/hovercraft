ffd529040751a1ed4d771199882971673ec0acbe
Calvin Owen <calvin.owen27@gmail.com>
2023-06-10 01:26:36 -0700

    Adding sprinting and improved control system

--- include/game/Player.h
+++ include/game/Player.h
@@ -3,15 +3,19 @@
 
 // #include "Game.h"
 #include "Object.h"
+#include <map>
 
 class Player : public Object
 {
     public:
-        float moveSpeed = 5;
+        float moveSpeed;
+        float runSpeed = 7.5;
+        float walkSpeed = 3.5;
 
         Player();
         void draw() override;
         void update(float time) override;
+        void processInputs();
 };
 
 #endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,62 +1,25 @@
 #include "../include/game/Player.h"
 #include "../include/game/Game.h"
 
-std::map<std::string, int> inputs;
+std::map<SDL_Scancode, bool> inputState;
 
 Player::Player() : Object("./content/smiley.png", Vector2(0, 0), Vector2(0.5, 0.5))
 {
-
 }
 
 void Player::update(float time)
 {
     Vector2 dir;
 
-    SDL_Event event;
-    while(game->inputEvents.size())
-    {
-        event = game->inputEvents.front();
+    processInputs();
 
-        if(event.key.keysym.scancode == game->keybinds["up"])
-        {
-            inputs["up"] = event.type == SDL_KEYDOWN?1:0;
-        }
-        if(event.key.keysym.scancode == game->keybinds["down"])
-        {
-            inputs["down"] = event.type == SDL_KEYDOWN?1:0;
-        }
-
-        if(event.key.keysym.scancode == game->keybinds["right"])
-        {
-            inputs["right"] = event.type == SDL_KEYDOWN?1:0;
-        }
-        if(event.key.keysym.scancode == game->keybinds["left"])
-        {
-            inputs["left"] = event.type == SDL_KEYDOWN?1:0;
-        }
-
-        game->inputEvents.pop();
-    }
-
-    if(inputs["up"])
-    {
-        dir.y += 1;
-    }
-    if(inputs["down"])
-    {
-        dir.y -= 1;
-    }
-
-    if(inputs["right"])
-    {
-        dir.x += 1;
-    }
-    if(inputs["left"])
-    {
-        dir.x -= 1;
-    }
-
-    // std::cout << dir << std::endl;
+    if(inputState[SDL_SCANCODE_W]) dir.y += 1; // up
+    if(inputState[SDL_SCANCODE_S]) dir.y -= 1; // down
+    if(inputState[SDL_SCANCODE_D]) dir.x += 1; // right
+    if(inputState[SDL_SCANCODE_A]) dir.x -= 1; // left
+    
+    moveSpeed = walkSpeed;
+    if(inputState[SDL_SCANCODE_LSHIFT]) moveSpeed = runSpeed; // run
 
     dir.normalize();
     velocity = dir * moveSpeed;
@@ -69,4 +32,17 @@ void Player::update(float time)
 void Player::draw()
 {
     Object::draw();
+}
+
+void Player::processInputs()
+{
+    SDL_Event event;
+    while(game->inputEvents.size())
+    {
+        event = game->inputEvents.front();
+
+        inputState[event.key.keysym.scancode] = (event.type == SDL_KEYDOWN);
+
+        game->inputEvents.pop();
+    }
 }
